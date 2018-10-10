f66784c293d077463e68d80876ffe8534e0ae358
thaipad <me@thaipad.pro>
2018-10-10 15:04:33 +0700

    Trying of ReactJS project

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About task manager</h1>
+      <p className="lead">made by Thaipad</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+}
